9787425b63f03271a0aa7f8b7610c5cccba8ef2d
Kaelan M-S <kaelan.ms@gmail.com>
2020-03-19 17:39:32 -0400

    fixed a bunch of stuff

--- main/main.ino
+++ main/main.ino
@@ -7,19 +7,21 @@
 
 void setup() {
   // Sets the two pins as Outputs
+  Serial.begin(9600);
+  Serial.println("Setup");
   pinMode(STEP_PIN,OUTPUT); 
   pinMode(DIRECTION_PIN,OUTPUT);
   pinMode(MOTOR_SPEED_PIN,OUTPUT);
-  pinMode(SWITCH_PIN,INPUT);
+  pinMode(SWITCH_PIN,INPUT_PULLUP);
 }
 
 char getNextCharInput(){
+  /* Serial.println("Waiting for input"); */
   char c;
-  while(c != endOfPacketMarker) {
-     if(Serial.available() > 0) {
-        c = Serial.read();
-     }
+  /* while(c != '\n') { */
+  while(Serial.available() == 0) {
   }
+  c = Serial.read();
   return c;
 }
 
@@ -27,23 +29,38 @@ void loop() {
   char input = 'h';
   while(input != 'q'){
     input = getNextCharInput();
-    if (input == 'h'){
+    Serial.print("Input = ");
+    Serial.println(input);
+    if (input == 't'){
+      Serial.println("Starting test mode");
+      for(int i=0; i<=5000; i++){
+        /* digitalWrite(STEP_PIN, HIGH); */
+        /* delayMicroseconds(500); */
+        /* digitalWrite(STEP_PIN, LOW); */
+        delayMicroseconds(500);
+        Serial.println(digitalRead(SWITCH_PIN));
+      }
+      Serial.println("Done");
+    }
+
+    else if (input == 'h'){
+      Serial.println("Starting home mode");
       home();
-      int numMStepsBack = backUpABit(numMStepsToBackUpALot);
+      int numMStepsBack = backUp(numMStepsToBackUpALot);
       mSteps += numMStepsBack;
     }
 
     else if (input == 'm'){
+      Serial.println("Starting measurement mode");
       int measurement = measure();
       Serial.print("Made a measurement of ");
       Serial.print(measurement);
       Serial.print(" microsteps which is ");
       Serial.print(measurement*MM_PER_MICROSTEP);
-      Serial.println(" mm.")
+      Serial.println(" mm.");
       Serial.println("Backing up a little then waiting for next input");
-      backUpABit(numMStepsToBackUpALittle);
+      backUp(numMStepsToBackUpALittle);
     }
     delay(200);
   }
-  return;
 }
--- main/measure.h
+++ main/measure.h
@@ -6,19 +6,22 @@ const int SWITCH_PIN = 6;
 
 int mSteps=0;
 
-const int numMStepsToBackUpALittle = 200*16;
-const int numMStepsToBackUpALot = 2000*16;
-const float MM_PER_MICROSTEP = 1234567; // TODO update
+const int STEPS_PER_MM = 200;
+const int MICROSTEPS_PER_STEP = 16;
+const float MICROSTEPS_PER_MM = STEPS_PER_MM * MICROSTEPS_PER_STEP;
+const float MM_PER_MICROSTEP = 1.0 / MICROSTEPS_PER_MM;
+const int numMStepsToBackUpALittle = 5*MICROSTEPS_PER_MM;
+const int numMStepsToBackUpALot = 150*MICROSTEPS_PER_MM;
 
 int forwardUntilHit(bool slow){
-  digitalWrite(DIRECTION_PIN,LOW);
+  digitalWrite(DIRECTION_PIN,HIGH);
   if (slow){
     digitalWrite(MOTOR_SPEED_PIN,HIGH);
   }
   else{
     digitalWrite(MOTOR_SPEED_PIN,LOW);
   }
-  while(digitalRead(SWITCH_PIN)==LOW){
+  while(digitalRead(SWITCH_PIN)==0){
     digitalWrite(STEP_PIN, HIGH);
     if (slow){
       delayMicroseconds(300);
@@ -37,7 +40,7 @@ int forwardUntilHit(bool slow){
 int backUp(int numMStepsToBackUp){
   int mStepsBackwards = 0;
   digitalWrite(MOTOR_SPEED_PIN,LOW);
-  digitalWrite(DIRECTION_PIN,HIGH);
+  digitalWrite(DIRECTION_PIN,LOW);
   while(mStepsBackwards < numMStepsToBackUp){
     digitalWrite(STEP_PIN, HIGH);
     delayMicroseconds(500);
@@ -46,16 +49,6 @@ int backUp(int numMStepsToBackUp){
     mStepsBackwards+=16;
   }
   return mStepsBackwards;
-}
-
-void home(){
-  // secretly just uses measure
-  Serial.println("starting home");
-  int howFarFromHomeWereWe = measure();
-  Serial.print("We were ");
-  Serial.print(howFarFromHomeWereWe);
-  Serial.print(" mSteps from home");
-  mSteps=0;
 }
 
 int measure(){
@@ -75,7 +68,7 @@ int measure(){
   
   delay(500);
 
-  int mStepsBackwards = backUpABit(numMStepsToBackUpALittle);
+  int mStepsBackwards = backUp(numMStepsToBackUpALittle);
 
   Serial.print("mStepsBackwards = ");
   Serial.println(mStepsBackwards);
@@ -99,3 +92,14 @@ int measure(){
   delay(200);
   return mSteps;
 }
+
+void home(){
+  // secretly just uses measure
+  Serial.println("starting home");
+  int howFarFromHomeWereWe = measure();
+  Serial.print("We were ");
+  Serial.print(howFarFromHomeWereWe);
+  Serial.print(" mSteps from home");
+  mSteps=0;
+}
+
